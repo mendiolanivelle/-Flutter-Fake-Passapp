1f8b1b65d15a400d37ed2d4a0dbbd70dec35e313
Nivelle Mendiola <mendiolanivelle@gmail.com>
2020-04-22 17:44:26 +0800

    [Flutter Fake Passapp] - Sign up page front end 100%

--- lib/page/Signup.dart
+++ lib/page/Signup.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+
 class Signup extends StatefulWidget {
   @override
   _SignupState createState() => _SignupState();
@@ -7,138 +8,115 @@ class Signup extends StatefulWidget {
 class _SignupState extends State<Signup> {
   @override
   Widget build(BuildContext context) {
-
-    List <String> headerNumber = ['+63'];
-    String inputnumbertip = 'Enter Phone Number';
+    List<String> headerNumber = ['+63'];
 
     return Scaffold(
       backgroundColor: Colors.white,
-
-      body: Center
-      (
+      body: Center(
         child: Padding(
           padding: const EdgeInsets.fromLTRB(0, 100, 0, 0),
-          child: Column
-          (
-            children: <Widget>
-            [
-              SizedBox
-              (
-                height: 100,
-                width: 260,
-                child: Container
-                (
-                  decoration: BoxDecoration
-                  (
-                    image: DecorationImage
-                    (
+          child: Column(
+            children: <Widget>[
+              SizedBox(
+                  height: 100,
+                  width: 260,
+                  child: Container(
+                    decoration: BoxDecoration(
+                        image: DecorationImage(
                       image: AssetImage('asset/img/passapp signin logo.png'),
-                    )
-                  ),
-                )
-              ),
-              Text
-              (
+                    )),
+                  )),
+              Text(
                 'Quarantine Hero',
-                style: TextStyle
-                (
+                style: TextStyle(
                   color: Colors.black,
                   fontSize: 38,
                   fontFamily: 'normMed',
-                  
                 ),
               ),
-              SizedBox(height: 100,),
               SizedBox(
-              width: 300,
-              height: 45,
-               child:Container(
-                 decoration: BoxDecoration
-                 (
-                   border: Border.all(color: Colors.grey),
-                   borderRadius: BorderRadius.all(Radius.circular(2))
-                 ),
-                 
-                 child: Row
-                  (
-                    children: <Widget>
-                    [
+                height: 100,
+              ),
+              SizedBox(
+                width: 320,
+                height: 50,
+                child: Container(
+                  decoration: BoxDecoration(
+                      border: Border.all(color: HSVColor.fromAHSV(1, 0, 0, 0.93).toColor()),
+                      borderRadius: BorderRadius.all(Radius.circular(2))),
+                  child: Row(
+                    children: <Widget>[
                       Padding(
-                        padding: const EdgeInsets.fromLTRB(20, 0, 0, 0),
-                        child: DropdownButton<String>
-                        (
-                          iconDisabledColor: Colors.black,
-                          hint: Text('+63',style: TextStyle(color: Colors.black),),
-                          items: headerNumber.map((String dropDownString) 
-                          {
-                            return DropdownMenuItem
-                            (
-                              value: dropDownString,
-                              child:Text(dropDownString),
-
-
-                            );
-                          }).toList(),
-
-                          
-                          value: '+63',
-                          
-
+                        padding: const EdgeInsets.fromLTRB(15, 0, 0, 0),
+                        child: DropdownButtonHideUnderline(
+                          child: DropdownButton<String>(
+                            iconDisabledColor: Colors.black,
+                            icon: Icon(Icons.keyboard_arrow_down),
+                            hint: Text(
+                              '+63',
+                              style: TextStyle(color: Colors.black,fontFamily: 'normMed',fontWeight: FontWeight.w900,fontSize: 16),
+                            ),
+                            items: headerNumber.map((String dropDownString) {
+                              return DropdownMenuItem(
+                                value: dropDownString,
+                                child: Text(dropDownString),
+                              );
+                            }).toList(),
+                          ),
                         ),
                       ),
-                      VerticalDivider
-                      (
+                      VerticalDivider(
                         width: 20,
                         thickness: 1,
                         indent: 6,
                         endIndent: 6,
                       ),
-
-                      Flexible
-                      (
-                        child: TextField
-                        (
+                      Flexible(
+                          child: TextField(
+                          style: TextStyle(fontFamily: 'normMed',fontWeight: FontWeight.w900),
                           keyboardType: TextInputType.number,
-                          decoration: InputDecoration
-                          (
-                            hintText: ('Enter Phone Number'),
-                            border: InputBorder.none,
-                            focusedBorder: InputBorder.none,
-                            enabledBorder: InputBorder.none,
-                            errorBorder: InputBorder.none,
-                            disabledBorder: InputBorder.none,
-
-                          ),
-                          onTap: ()
-                          {
-                            setState(() {
-                              inputnumbertip = '';
-                            });
-                            
-                          },
-                        )
+                          decoration: InputDecoration(
+                          hintStyle: TextStyle(fontFamily: 'normReg',fontWeight: FontWeight.w100,fontSize: 18,color: HSVColor.fromAHSV(1, 0, 0, 0.44).toColor()),
+                          hintText: ('Enter Phone Number'),
+                          border: InputBorder.none,
+                          focusedBorder: InputBorder.none,
+                          enabledBorder: InputBorder.none,
+                          errorBorder: InputBorder.none,
+                          disabledBorder: InputBorder.none,
                         ),
+                        onTap: () {},
+                      )),
                     ],
                   ),
-               ),
-              ), 
-              SizedBox(height: 80,),
-              ButtonTheme
-              (
-                minWidth: 300,
+                ),
+              ),
+              SizedBox(
+                height: 60,
+              ),
+              ButtonTheme(
+                minWidth: 320,
                 height: 50,
-              child:FlatButton
+                child: FlatButton(
+                  onPressed: () {},
+                  color: HSVColor.fromAHSV(1, 0, 0, 0.19).toColor(),
+                  child: Text(
+                    'Login',
+                    style: TextStyle(color: Colors.white, fontSize: 19,fontFamily: 'normMed'),
+                  ),
+                ),
+              ),
+              SizedBox(height: 20,),
+              GestureDetector
               (
-                onPressed: () {},
-                color: HSVColor.fromAHSV(1, 0, 0, 0.19).toColor(),
-                child: Text('Login',style: TextStyle(color: Colors.white,fontSize: 18),),
+                onTap: () {},
+                child: Text('Sign Up',style: TextStyle(fontFamily: 'normMed',fontSize: 19,decoration: TextDecoration.underline),),
               ),
-              ),
+              SizedBox(height: 100,),
+              Text('V 1.0.1.13',style: TextStyle(fontFamily: 'normMed',fontWeight: FontWeight.w900,fontSize: 18),)
             ],
           ),
         ),
       ),
-      
     );
   }
 }
