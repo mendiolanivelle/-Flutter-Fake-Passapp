49dc16dabe4631c2d336ba00542a1790f6af61e6
Nivelle Mendiola <mendiolanivelle@gmail.com>
2020-05-01 04:06:10 +0800

    [Flutter Fake Passapp] - home page is now at 80%

--- lib/page/Home.dart
+++ lib/page/Home.dart
@@ -9,9 +9,78 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('nivelle'),
-      ),
+      body: SafeArea(
+          child: Container(
+        padding: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+        child: Column(
+          children: <Widget>[
+            Row(mainAxisSize: MainAxisSize.max, children: <Widget>[
+              Expanded(
+                child: Icon(
+                  Icons.menu,
+                  size: 48,
+                ),
+                flex: 1,
+              ),
+              SizedBox(
+                width: 10,
+              ),
+              Expanded(
+                child: Text(
+                  'CODE-ABCD',
+                  style: TextStyle(fontSize: 34, fontWeight: FontWeight.bold),
+                ),
+                flex: 5,
+              ),
+            ]),
+            Container(
+                height: 350,
+                width: 350,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    image: DecorationImage(
+                        image: AssetImage(
+                          'asset/img/passapp home CEO.png',
+                        ),
+                        fit: BoxFit.cover),
+                    border: Border.all(
+                        width: 10,
+                        color: Colors.lightGreen,
+                        style: BorderStyle.solid))),
+            SizedBox(
+              height: 5,
+            ),
+            Row(
+              mainAxisSize: MainAxisSize.max,
+              children: <Widget>[
+                Expanded(
+                  flex: 1,
+                  child: FlatButton(
+                    onPressed: () {},
+                    child: Text(
+                      'Primary',
+                      style:
+                          TextStyle(fontSize: 21, fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                ),
+                Expanded(
+                  flex: 1,
+                  child: FlatButton(
+                    onPressed: () {},
+                    child: Text(
+                      'More',
+                      style:
+                          TextStyle(fontSize: 21, fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                )
+              ],
+            ),
+            Container()
+          ],
+        ),
+      )),
     );
   }
 }
